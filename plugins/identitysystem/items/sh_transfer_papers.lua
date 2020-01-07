702f3666aad28b60e2270188381e170ae503185b
Noiselessy <ganadoskuroi@gmail.com>
2020-01-07 01:48:49 -0300

    .

---
+++ plugins/identitysystem/items/sh_transfer_papers.lua
@@ -0,0 +1,25 @@
+ITEM.name = "Cartão de Transferência"
+ITEM.model = Model("models/props_bts/bts_clipboard.mdl")
+ITEM.description = "Um folheto de papel indicando suas informações de transferência."
+
+function ITEM:GetDescription()
+	return self.description
+end
+function ITEM:PopulateTooltip(tooltip)
+	local data = tooltip:AddRow("data")
+	data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+	data:SetText("Nome: " .. self:GetData("citizen_name", "Unissued") .. "\nNº de transferência: " .. self:GetData("unique", "00000"))
+	data:SetFont("BudgetLabel")
+	data:SetExpensiveShadow(0.5 )
+	data:SizeToContents()
+
+	local data2 = tooltip:AddRow("data2")
+	data2:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+	data2:SetText("Entregue estes papéis para um oficial e receba um CID. ISTO NÃO É UMA IDENTIFICAÇÃO VÁLIDA!.")
+	data2:SetFont("DermaDefault")
+	data2:SetExpensiveShadow(0.5)
+	data2:SizeToContents()
+
+
+
+end
