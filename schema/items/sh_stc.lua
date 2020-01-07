3fcf789cebfa0897ebf1cb07f9265021d0ae431f
Noiselessy <37081000+Noiselessy@users.noreply.github.com>
2020-01-06 22:59:56 -0300

    Update sh_stc.lua

--- schema/items/sh_stc.lua
+++ schema/items/sh_stc.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "STC"
-ITEM.model = Model("models/gibs/metal_gib4.mdl")
+ITEM.model = Model("models/sky/cwuid.mdl")
 ITEM.description = "Um cartão usado para identificação de membros do sindicato com o ID #%s, atribuído a %s."
 
 function ITEM:GetDescription()
