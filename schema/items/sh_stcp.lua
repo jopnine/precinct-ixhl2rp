92c35a52be6f87543855bcf0aaf000b12df3ebd6
Noiselessy <37081000+Noiselessy@users.noreply.github.com>
2020-01-06 23:03:27 -0300

    Update sh_stcp.lua

--- schema/items/sh_stcp.lua
+++ schema/items/sh_stcp.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "STCP"
-ITEM.model = Model("models/dorado/tarjetazero.mdl")
+ITEM.model = Model("models/dorado/tarjeta3.mdl")
 ITEM.description = "Um cartão usado para identificação de membros do setor de pesquisa e desenvolvimento do sindicato com o ID #%s, atribuído a %s."
 
 function ITEM:GetDescription()
